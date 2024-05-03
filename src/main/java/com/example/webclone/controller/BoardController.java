0937574678180a386b29478b7c5279fb6bd58953
Dongwook Kim <dongwook258@naver.com>
2024-05-04 00:22:03 +0900

    half complete

--- src/main/java/com/example/webclone/controller/BoardController.java
+++ src/main/java/com/example/webclone/controller/BoardController.java
@@ -1,10 +1,12 @@
 package com.example.webclone.controller;
 
+import org.springframework.ui.Model;
 import com.example.webclone.entity.Board;
 import com.example.webclone.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -22,17 +24,17 @@ public class BoardController {
         return "boardMain";
     }
 
-    @GetMapping("/김동욱")
+    @GetMapping("/kdw")
     public String boardKDW() {
         return "boardKDW";
     }
 
-    @GetMapping("/김동욱/board/write")
+    @GetMapping("/kdw/board/write")
     public String boardWrite() {
         return "boardWrite";
     }
 
-    @PostMapping("/김동욱/board/writepro")
+    @PostMapping("/kdw/board/writepro")
     public String boardWritePro(Board board) {
 
         boardService.write(board);
@@ -40,10 +42,48 @@ public class BoardController {
         return "";
     }
 
-    @GetMapping("/김동욱/board/list")
+    @GetMapping("/kdw/board/list")
     public String boardList(Model model) {
 
+        model.addAttribute("list", boardService.boardList());
 
         return "boardList";
+    }
+
+    @GetMapping("/kdw/board/view") // local:8080/kdw/board/view?id=1
+    public String boardView(Model model, Integer id) {
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardView";
+    }
+
+
+    @GetMapping("/kdw/board/delete")
+    public String boardDelete(Integer id) {
+
+        boardService.boardDelete(id);
+
+        return "redirect:/kdw/board/list";
+    }
+
+    @GetMapping("/kdw/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardModify";
+    }
+
+    @PostMapping("/kdw/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/kdw/board/list";
     }
 }
